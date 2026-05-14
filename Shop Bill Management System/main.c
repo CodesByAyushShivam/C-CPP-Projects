01f0d4dedc080d49ac6fa40ab9a5f24cb50116a3
Ayush Shivam <ayushshivam7245@gmail.com>
2026-05-14 15:07:21 +0530

    Refactor item structure and add GST calculation
    Updated the item structure to use float for price and added GST calculation functions.

--- Shop Bill Management System/main.c
+++ Shop Bill Management System/main.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
+#include <time.h>
 
 void printList();
-int price(int, float);
+float price(int, float);
+float calGst(float, int);
+int gstrate();
 
-typedef struct item
+struct item
 {
-    int price;
+    float price;
     char name[20];
     int id;
     float quant;
     int gst;
-} unique;
+};
 
 
 int main(){
     int n;
     printf("Enter the number of items: ");
+    // printf("Unix Timestamp: %ld\n", time(NULL));
     scanf("%d", &n);
     printList();
-    unique list[n];
-    int subTotal = 0;
+    struct item list[n];
+    struct item *ptr;
+    ptr = list;
     for (int i = 0; i < n; i++)
     {
         printf("Enter the id of the item %d: ", i+1);
@@ -27,10 +32,9 @@ int main(){
         printf("Enter the quantity item %d: ", i+1);
         scanf("%f", &list[i].quant);
         list[i].price = price(list[i].id, list[i].quant);
-        subTotal += list[i].price;
+        int gst = gstrate();
+        list[i].gst = calGst(list[i].price, gst);
     }
-
-    printf("%d", subTotal);
     
     return 0;
 }
@@ -46,7 +50,7 @@ void printList(){
     printf("7. Egg\t\t Price: 82\n");
 }
 
-int price(int id, float quant){
+float price(int id, float quant){
     switch(id){
         case 1:{
             return quant*70;
@@ -76,5 +80,53 @@ int price(int id, float quant){
             return quant*82;
             break;
         }
+    }
+}
+
+float calGst(float price, int rate){
+    return price * rate ;
+}
+
+int gstrate(){
+    int n;
+
+    printf("Choose GST rate for this product. Enter the serial number against the required GST to get it as input: \n 1. 5%c \n 2. 12%c \n 3. 18%c \n 4. 28%c \n 5. 40%c \n Any other value will result in inclusion of 0%c GST on the final bill !! \n Input:", '%', '%', '%', '%', '%', '%');
+    scanf("%d", &n);
+    switch (n){
+    case 1:{
+        printf("5 %c applied on your product \n", '%');
+        return 5;
+        break;
+    }
+    
+    case 2:{
+        printf("12 %c applied on your product \n", '%');
+        return 12;
+        break;
+    }
+    
+    case 3:{
+        printf("18 %c applied on your product \n", '%');
+        return 18;
+        break;
+    }
+    
+    case 4:{
+        printf("28 %c applied on your product \n", '%');
+        return 28;
+        break;
+    }
+    
+    case 5:{
+        printf("40 %c applied on your product \n", '%');
+        return 40;
+        break;
+    }
+    
+    default:{
+        printf("No applied on your product \n");
+        return 0;
+        break;
+    }
     }
 }
